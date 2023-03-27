37b78cddb37aff93a91f621467e01e1c11921b20
EuHiroshi <hiroshivz001@gmail.com>
2023-03-27 17:31:35 -0300

    Projeto pokedex

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,8 @@ import {writeFile} from 'fs/promises'
 import pokemon from './server.js'
 
 async function main() {
-    pokemon.getApi();
+    pokemon.getApi()
+    
 }
 
 main();
--- JS/server.js
+++ JS/server.js
@@ -4,9 +4,11 @@ class pokemon {
 
     async getApi() {
         try {
-            const pokeApi = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
+            const pokeApi = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+                .then( res => {
+                    return res.json()
+                });
             const createJson = await writeFile('pokemons.json', JSON.stringify(pokeApi, null, 2))
-            console.log('cheguei')
             
         } catch (error) {
             console.log('erro')
